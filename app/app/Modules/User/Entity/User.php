ac382653d6d9b99b3dbd455ea2d70bda1b693bb7
Alex Novak <alexleonnovak@gmail.com>
2020-04-18 11:23:11 +0300

    gitignore

--- app/app/Modules/User/Entity/User.php
+++ app/app/Modules/User/Entity/User.php
@@ -4,7 +4,22 @@
 namespace App\Modules\User\Entity;
 
 
-class User
-{
+use App\Http\Middleware\Authenticate;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+use Ramsey\Uuid\Uuid;
 
+class User extends Authenticatable
+{
+    private string $id;
+    private ?string $name;
+    private string $status;
+    private string $email;
+    private string $passwordHash;
+
+    public function create()
+    {
+        $this->id = Str::uuid();
+    }
 }
